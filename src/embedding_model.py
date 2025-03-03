ab6bef3f38ef59c66f9e446a95146a8f564263c6
Bruno Evangelista <bpevangelista@gmail.com>
2025-03-02 16:00:06 -0800

    Fixed embedding generation

--- src/embedding_model.py
+++ src/embedding_model.py
@@ -1,9 +1,17 @@
+import gc
 import torch
 from transformers import AutoModel, AutoTokenizer
 
 from logging_config import get_logger
 
 logger = get_logger(__name__)
+
+
+def clear_cuda_caches():
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        torch.cuda.ipc_collect()
+    gc.collect()
 
 
 def _try_load_model(model_name_or_path: str):
@@ -30,22 +38,35 @@ def _try_load_tokenizer(model_name_or_path: str, max_length: int):
 
 class EmbeddingModel:
     MODEL_ID = "codesage/codesage-large-v2"
-    MODEL_CONTEXT_LENGTH = 4096
+    MODEL_CONTEXT_LENGTH = 1024
 
     def __init__(self):
+        clear_cuda_caches()
+
         self.model = _try_load_model(self.MODEL_ID)
         self.tokenizer = _try_load_tokenizer(self.MODEL_ID, self.MODEL_CONTEXT_LENGTH)
         self.device = next(self.model.parameters()).device
 
     def generate(self, text: str):
         logger.info(f"Generate: {text}")
-        inputs = self.tokenizer.encode(
-            text, return_tensors="pt", truncation=True, padding=True
-        ).to(self.device)
 
+        # [B, D_MODEL]
+        inputs = self.tokenizer(text, return_tensors="pt", truncation=True, padding=True).to(self.device)
         with torch.no_grad():
-            model_output = self.model(inputs)
+            outputs = self.model(
+                input_ids=inputs.input_ids,
+                attention_mask=inputs.attention_mask,
+                return_dict=True,
+            )
 
-        embeddings = model_output[0]
-        logger.info(f"Generate: {model_output[0].shape}")
-        return embeddings.tolist()
+        # Get or compute pool output
+        if hasattr(outputs, "pooler_output") and outputs.pooler_output is not None:
+            embedding = outputs.pooler_output
+        else:
+            # Mean pooling
+            embedding = outputs.last_hidden_state.mean(dim=1)
+
+        # Normalize vector
+        embedding = torch.nn.functional.normalize(embedding, p=2, dim=1)
+        embedding = embedding.cpu().numpy()
+        return embedding.tolist()
---
+++ src/test.sh
@@ -0,0 +1,1 @@
+curl localhost:8000/v1/embeddings -H "Content-Type: application/json" -d '{ "prompt": "def fibonnaci(n): return 0" }'
